623d037530ad2683aefc5e5970676e2872720d2c
DmitryIgnatev <dmitryignatevdev@gmail.com>
2024-08-19 14:44:50 +0400

    feat: сделал скролл при перемещении draggable элемента

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class TaskBoardScreen extends StatefulWidget {
 }
 
 class _TaskBoardScreenState extends State<TaskBoardScreen> {
+  final double _scrollSpeed = 10.0;
   final ScrollController _scrollController = ScrollController();
   final List<List<String>> columns = [
     ['Task 1', 'Task 2'],
@@ -33,74 +34,92 @@ class _TaskBoardScreenState extends State<TaskBoardScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final scrollThreshold = width / 6;
     return Scaffold(
       appBar: AppBar(title: const Text("Task Board")),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: (ScrollNotification notification) {
-          if (notification is OverscrollNotification) {
-            if (notification.overscroll < 0 && _scrollController.offset <= 0) {
-              // Left edge reached
-              _scrollController.jumpTo(0);
-            } else if (notification.overscroll > 0 &&
-                _scrollController.offset >=
-                    _scrollController.position.maxScrollExtent) {
-              // Right edge reached
-              _scrollController
-                  .jumpTo(_scrollController.position.maxScrollExtent);
-            }
-          }
-          return false;
-        },
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          controller: _scrollController,
-          itemCount: columns.length,
-          itemBuilder: (context, columnIndex) {
-            return DragTarget<String>(
-              onAcceptWithDetails: (task) {
-                setState(() {
-                  // Remove the task from its previous column
-                  for (var column in columns) {
-                    column.remove(task.data);
-                  }
-                  // Add the task to the new column
-                  columns[columnIndex].add(task.data);
-                });
-              },
-              builder: (context, candidateData, rejectedData) {
-                return Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)),
-                  margin: const EdgeInsets.all(8),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Column ${columnIndex + 1}',
-                            style: const TextStyle(fontSize: 18)),
-                        for (var task in columns[columnIndex])
-                          Draggable<String>(
-                            data: task,
-                            feedback: Material(
-                              child: TaksCard(task: task, isDragging: true),
-                            ),
-                            childWhenDragging: Container(),
-                            child: TaksCard(task: task),
+      body: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        controller: _scrollController,
+        itemCount: columns.length,
+        itemBuilder: (context, columnIndex) {
+          return DragTarget<String>(
+            onAcceptWithDetails: (task) {
+              setState(() {
+                // Remove the task from its previous column
+                for (var column in columns) {
+                  column.remove(task.data);
+                }
+                // Add the task to the new column
+                columns[columnIndex].add(task.data);
+              });
+            },
+            builder: (context, candidateData, rejectedData) {
+              return Container(
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(12)),
+                margin: const EdgeInsets.all(8),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Column ${columnIndex + 1}',
+                          style: const TextStyle(fontSize: 18)),
+                      for (var task in columns[columnIndex])
+                        Draggable<String>(
+                          data: task,
+                          feedback: Material(
+                            child: TaksCard(task: task, isDragging: true),
                           ),
-                      ],
-                    ),
+                          onDragUpdate: (details) {
+                            debugPrint('${details.globalPosition}');
+                            debugPrint('$width');
+                            if (details.globalPosition.dx >
+                                width - scrollThreshold) {
+                              _scrollRight();
+                            }
+                            if (details.globalPosition.dx < scrollThreshold) {
+                              _scrollLeft();
+                            }
+                          },
+                          childWhenDragging: Container(),
+                          child: TaksCard(task: task),
+                        ),
+                    ],
                   ),
-                );
-              },
-            );
-          },
-        ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
+  }
+
+  void _scrollRight() {
+    if (_scrollController.position.pixels <
+        _scrollController.position.maxScrollExtent) {
+      _scrollController.animateTo(
+        _scrollController.position.pixels + _scrollSpeed,
+        duration: const Duration(milliseconds: 50),
+        curve: Curves.linear,
+      );
+    }
+  }
+
+  void _scrollLeft() {
+    if (_scrollController.position.pixels >
+        _scrollController.position.minScrollExtent) {
+      _scrollController.animateTo(
+        _scrollController.position.pixels - _scrollSpeed,
+        duration: const Duration(milliseconds: 50),
+        curve: Curves.linear,
+      );
+    }
   }
 }
 
